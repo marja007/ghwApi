0668c7c250226a09063c3f6eaf5d195679fd3bf5
marja007 <marjanmainosposti@gmail.com>
2023-04-04 19:37:03 +0300

    added refresh

--- app.py
+++ app.py
@@ -2,4 +2,7 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def hello_ghw():
-    return"Hello"
+    return"<p>Hello You gorgeous<p>"
+
+if __name__=="__main_2":
+    app.run(debug=True)
